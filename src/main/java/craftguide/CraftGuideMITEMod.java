4b4e6cda84ff8221c00ef863f1543d5d4b12f072
limingzxc <2904962732@qq.com>
2024-03-24 21:07:52 +0800

    only client

--- src/main/java/craftguide/CraftGuideMITEMod.java
+++ src/main/java/craftguide/CraftGuideMITEMod.java
@@ -1,51 +1,16 @@
 package craftguide;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
-import net.xiaoyu233.fml.AbstractMod;
-import net.xiaoyu233.fml.classloading.Mod;
-import net.xiaoyu233.fml.config.InjectionConfig;
 
-import javax.annotation.Nonnull;
-
-@Mod
-public class CraftGuideMITEMod extends AbstractMod implements ModInitializer, PreLaunchEntrypoint {
+public class CraftGuideMITEMod implements ClientModInitializer, PreLaunchEntrypoint {
     public static String modId = "craftguide";
     public static int modVerNum = 1000;
     public static String modVerStr = "v1.0.0";
     public static String modName = "CraftGuide Mod";
 
     @Override
-    public void preInit() {
-
-    }
-
-    @Nonnull
-    @Override
-    public InjectionConfig getInjectionConfig() {
-        return null;
-    }
-
-    @Override
-    public void postInit() {}
-
-    @Override
-    public String modId() {
-        return modId;
-    }
-
-    @Override
-    public int modVerNum() {
-        return modVerNum;
-    }
-
-    @Override
-    public String modVerStr() {
-        return modVerStr;
-    }
-
-    @Override
-    public void onInitialize() {}
+    public void onInitializeClient() {}
 
     @Override
     public void onPreLaunch() {
